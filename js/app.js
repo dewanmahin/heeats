3d908a899fb96c942fa6b600d3f02632800a9588
DEWAN <dewanmahinchy@gmail.com>
2023-03-10 00:37:47 +0600

    display temerature dynamically

--- js/app.js
+++ js/app.js
@@ -2,9 +2,15 @@
 const api_key = `1ba2aed6b07ba63ff2fcf7a2e0543a83`;
 
 const loadTemperature = async city => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    displayTemperature(data);
 }
+
+const displayTemperature = data => {
+    const temperature = document.getElementById('temperature');
+    temperature.innerText = data.main.temp;
+}
+
 loadTemperature('new york')
